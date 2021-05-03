1392a9af17b864c68a108b48b84e9e22e28d842c
Xavi <xavi@geekshubs.com>
2021-05-03 11:15:42 +0200

    change microservice name to patients

--- microservice/src/main/resources/db/migration/V1.1__CreateTablePatients.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS PATIENTS(
-    uuid varchar(100) NOT NULL,
-    name varchar(10) NOT NULL,
-    lastName varchar(10) NOT NULL,
-    dateCreated DATE,
-    birthDate DATE,
-    PRIMARY KEY (uuid )
-    );
